fe090e876bf21bbd1479ca2a4e8a1e5c57e06d23
heboan <heboan@qq.com>
2020-04-05 22:48:21 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -7,7 +7,7 @@ def Println(content){
 
 //格式化输出
 def PrintMsg(value, color){
-    color = ['red'   : "\003[40;31m >>>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<<< \033[0m",
+    colors = ['red'   : "\003[40;31m >>>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<<< \033[0m",
              'blue'  : "\003[47;34m ${value} \033[0m",
              'green' : "\033[40;32m >>>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<<< \033[0m"]
     ansiColor('xterm'){
